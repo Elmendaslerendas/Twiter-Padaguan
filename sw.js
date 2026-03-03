17a0c33f1fb896890592cae97d4de59402bb66ef
Elmendaslerendas <luis@kaixo.com>
2026-03-03 21:22:30 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,30 @@
-const CACHE_NAME = 'padaguan-v1';
+const CACHE_NAME = 'padaguan-v2';
 const ASSETS = [
     '/',
     '/index.html',
     '/style.css',
     '/app.js',
     '/config.js',
-    '/icon.png'
+    '/icon.png',
+    '/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
